6352da0893a7cf9e1ceebc0da8ffb847e045e0c6
ptma <ptma@163.com>
2023-12-26 17:16:22 +0800

    perf: 从脚本发布消息时，载荷支持多种类型的数据

--- src/main/java/com/mqttinsight/scripting/modules/MqttClientWrapper.java
+++ src/main/java/com/mqttinsight/scripting/modules/MqttClientWrapper.java
@@ -1,5 +1,10 @@
 package com.mqttinsight.scripting.modules;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.caoccao.javet.enums.V8ValueReferenceType;
+import com.caoccao.javet.exceptions.JavetException;
+import com.caoccao.javet.values.reference.V8ValueTypedArray;
 import com.mqttinsight.codec.CodecSupport;
 import com.mqttinsight.mqtt.Subscription;
 import com.mqttinsight.scripting.ScriptCodec;
@@ -7,12 +12,14 @@ import com.mqttinsight.scripting.ScriptPubMqttMessage;
 import com.mqttinsight.scripting.SimpleMqttMessage;
 import com.mqttinsight.ui.form.panel.MqttInstance;
 import com.mqttinsight.util.Utils;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.function.Function;
 
 /**
  * @author ptma
  */
+@Slf4j
 public class MqttClientWrapper {
 
     private final MqttInstance mqttInstance;
@@ -38,6 +45,10 @@ public class MqttClientWrapper {
         publish(topic, payload.getBytes(), 0, false);
     }
 
+    public void publish(String topic, V8ValueTypedArray payload) throws JavetException {
+        publish(topic, payload, 0, false);
+    }
+
     public void publish(String topic, byte[] payload) {
         publish(topic, payload, 0, false);
     }
@@ -46,12 +57,34 @@ public class MqttClientWrapper {
         publish(topic, payload.getBytes(), qos, false);
     }
 
+    public void publish(String topic, V8ValueTypedArray payload, int qos) throws JavetException {
+        publish(topic, payload, qos, false);
+    }
+
     public void publish(String topic, byte[] payload, int qos) {
         publish(topic, payload, qos, false);
     }
 
     public void publish(String topic, String payload, int qos, boolean retained) {
         publish(topic, payload.getBytes(), qos, retained);
+    }
+
+    public void publish(String topic, V8ValueTypedArray payload, int qos, boolean retained) throws JavetException {
+        byte[] bytes;
+        if (payload.getType() == V8ValueReferenceType.Uint8Array) {
+            JSONObject json = JSONUtil.parseObj(payload.toJsonString());
+            if ("Buffer".equals(json.getStr("type"))) {
+                bytes = json.getBytes("data");
+            } else {
+                bytes = payload.toBytes();
+            }
+        } else if (payload.getType() == V8ValueReferenceType.Int8Array) {
+            bytes = payload.toBytes();
+        } else {
+            log.warn("The type of the payload parameter \"{}\" is not supported.", payload.getType());
+            return;
+        }
+        publish(topic, bytes, qos, retained);
     }
 
     public void publish(String topic, byte[] payload, int qos, boolean retained) {
